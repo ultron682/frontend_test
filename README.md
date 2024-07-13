00c6a0c74be5a0ebad407638e0296d180dcdbd5e
ultron682 <52131708+ultron682@users.noreply.github.com>
2024-07-13 19:55:01 +0200

    cleanup

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 This project was generated with Angular CLI 18.1.0.
 
+I managed to do all the assumptions that were required along with the optional elements.
+In addition, I did simple unit tests of the main components.
 
+Best regards Michal Mazur
--- pomyslna-rekrutacja/src/app/components/main/container/main-container/main-container.component.spec.ts
+++ pomyslna-rekrutacja/src/app/components/main/container/main-container/main-container.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,12 +9,12 @@ describe('MainContainerComponent', () => {
   let component: MainContainerComponent;
   let fixture: ComponentFixture<MainContainerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ MainContainerComponent ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MainContainerComponent);
